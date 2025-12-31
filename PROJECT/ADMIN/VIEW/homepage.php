c468994c2f372f782d893817bcdab303e17188c6
fahimaislam2022 <23-50251-1@student.aiub.edu>
2025-12-31 18:08:16 +0600

    Add initial homepage layout for Hotel Management System

---
+++ PROJECT/ADMIN/VIEW/homepage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<head>
+    
+    
+    <title>Hotel Management System</title>
+    
+</head>
+<body>
+
+
+<header class="hero">
+    <div class="hero-content">
+        <h1>Hotel Management System</h1>
+        <p>Travel More, Spend Less</p>
+
+        <div class="button-group">
+            <a href="#" class="btn btn-primary">Sign In</a>
+            <a href="#" class="btn btn-secondary">Register</a>
+            <a href="login.php" class="btn btn-admin">Login as Admin</a>
+        </div>
+    </div>
+</header>
+
+
+</body>
+</html>
