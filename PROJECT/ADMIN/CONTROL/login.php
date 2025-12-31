e3194b9aabb3743d5c7b81ec8b1f04bab33729d8
fahimaislam2022 <23-50251-1@student.aiub.edu>
2025-12-31 21:35:08 +0600

    add control in log in

---
+++ PROJECT/ADMIN/CONTROL/login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+@include("../MODEL/db.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if (empty($username) || empty($password)) {
+        $error = "All fields are required!";
+    } else {
+        $sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $_SESSION["username"] = $username;
+
+            
+            if (isset($_POST["remember"])) {
+                setcookie("username", $username, time() + (86400 * 7), "/");
+                setcookie("password", $password, time() + (86400 * 7), "/");
+            } else {
+                setcookie("username", "", time() - 3600, "/");
+                setcookie("password", "", time() - 3600, "/");
+            }
+
+            header("Location: ../VIEW/dashboard.php");
+            exit();
+        } else {
+            $error = "Invalid username or password!";
+        }
+    }
+}
+?>
