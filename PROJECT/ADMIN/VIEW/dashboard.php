d2d78b022e48de8e16b17fd3efab806881394fae
fahimaislam2022 <23-50251-1@student.aiub.edu>
2026-01-01 10:41:39 +0600

    dashboard html basic

---
+++ PROJECT/ADMIN/VIEW/dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+    <title>HMS Dashboard</title>
+</head>
+<body>
+
+<div class="sidebar">
+    <h2 class="brand">Hotel Management System</h2>
+    <ul class="menu">
+        <li><a href="dashboard.php" class="active">Home</a></li>
+        <li><a href="rooms.php">Rooms</a></li>
+        <li><a href="housekeeping.php">Housekeeping</a></li>
+        <li><a href="customers.php">Customers Profiles</a></li>
+        <li><a href="booking.php">Booking & Billing</a></li>
+        <li><a href="promotions.php">Promotions</a></li>
+        <li><a href="changepassword.php">Change Password</a></li>
+        <li><a href="../Control/logout.php">Logout</a></li>
+    </ul>
+</div>
+</body>
+</html>
