14d38579e947af3f9f8ef47e4bd0b131008afe12
fahimaislam2022 <23-50251-1@student.aiub.edu>
2026-01-17 08:31:55 +0600

    housekeeping basic

---
+++ PROJECT/ADMIN/VIEW/housekeeping.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+include("../CONTROL/housekeeping_data.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HMS Housekeeping</title>
+    <link rel="stylesheet" href="../CSS/dashboard.css">
+</head>
+<body>
+
+<div class="sidebar">
+    <h2 class="brand">Hotel Management System</h2>
+    <ul class="menu">
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a href="rooms.php">Rooms</a></li>
+        <li><a href="housekeeping.php" class="active">Housekeeping</a></li>
+        <li><a href="customers.php">Customers</a></li>
+        <li><a href="booking.php">Booking</a></li>
+        <li><a href="billing.php">Billing</a></li>
+        <li><a href="changepassword.php">Change Password</a></li>
+        <li><a href="../CONTROL/logout.php">Logout</a></li>
+    </ul>
+</div>
+
+</body>
+</html>
