52710c19084dd1ca06f03349ad6d2364cfcbf2a7
fahimaislam2022 <23-50251-1@student.aiub.edu>
2026-01-17 09:04:49 +0600

    customer side bar view

---
+++ PROJECT/ADMIN/VIEW/customers.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>HMS Customers</title>
+    <link rel="stylesheet" href="../CSS/dashboard.css">
+    
+</head>
+<body>
+
+<div class="sidebar">
+    <h2 class="brand">Hotel Management System</h2>
+    <ul class="menu">
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a href="rooms.php">Rooms</a></li>
+        <li><a href="housekeeping.php">Housekeeping</a></li>
+        <li><a href="customers.php" class="active">Customers</a></li>
+        <li><a href="booking.php">Booking</a></li>
+        <li><a href="billing.php">Billing</a></li>
+        <li><a href="changepassword.php">Change Password</a></li>
+        <li><a href="../CONTROL/logout.php">Logout</a></li>
+    </ul>
+</div>
+
+</body>
+</html>
