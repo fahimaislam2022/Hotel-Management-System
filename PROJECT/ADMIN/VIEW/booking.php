41b433bb68352f6444ec9b65cfad31a8f0323b09
fahimaislam2022 <23-50251-1@student.aiub.edu>
2026-01-17 09:41:33 +0600

    booking html side bar

---
+++ PROJECT/ADMIN/VIEW/booking.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit();
+}
+include("../CONTROL/booking_data.php");
+?>
+<!DOCTYPE html>
+<head>
+    <title>HMS Booking</title>
+    <link rel="stylesheet" href="../CSS/dashboard.css">
+</head>
+<body>
+
+<div class="sidebar">
+    <h2 class="brand">Hotel Management System</h2>
+    <ul class="menu">
+        <li><a href="dashboard.php">Home</a></li>
+        <li><a href="rooms.php">Rooms</a></li>
+        <li><a href="housekeeping.php">Housekeeping</a></li>
+        <li><a href="customers.php">Customers</a></li>
+        <li><a href="booking.php" class="active">Booking</a></li>
+        <li><a href="Billing.php">Billing</a></li>
+        <li><a href="changepassword.php">Change Password</a></li>
+        <li><a href="../CONTROL/logout.php">Logout</a></li>
+    </ul>
+    <div class="user-mini">
+        <div class="user-info">
+            <h4><?php echo $_SESSION['username']; ?></h4>
+            <span>Manager</span>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
+
